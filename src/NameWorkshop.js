d58e58ed90e156c60d78e7d619dd1bb3fd9c0266
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2017-08-30 21:14:48 -0400

    eliminate a layer of component abstraction to make it simpler to provide the onclick callback

--- src/NameWorkshop.js
+++ src/NameWorkshop.js
@@ -3,23 +3,104 @@ import './NameWorkshop.css';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 class NameWorkshop extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.nameLength = 6;
+        /*
+        var weights = {};
+        for (var i=0; i < this.nameLength; i++) {
+            weights[i] = {};
+                0
+        }
+        */
+        var chosenLetters = new Array(this.nameLength).fill(null);
+        this.state = {
+            //weights: weights,
+            chosenLetters: chosenLetters
+        };
+        this.handleLetterClick = this.handleLetterClick.bind(this);
+        this.consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
+        this.vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+        this.colsPerLetter = 2;
+        // TODO for general name lengths: Math.max(Math.ceil(12.0/this.nameLength), 12);
+    }
+
+    letterArray(letter) {
+        return this.consonants.indexOf(letter) >= 0 ? this.consonants : this.vowels;
+    }
+
+    renderLetter(letter, columnIndex, letterIndex) {
+        var key = `${ letter }${ columnIndex }`;
+        return (
+            <ClickableLetter
+                letter = {letter}
+                onClick={this.handleLetterClick(letter, columnIndex, letterIndex)}
+                key={key}
+            >
+        );
+    }
+
+    renderBlank(columnIndex) {
+        var blankKey = `bl${columnIndex}`;
+        return (
+            <LetterBlank
+                selectedLetter={this.state.chosenLetters[columnIndex]}
+                key={blankKey}
+            />
+        );
+    }
+
+    renderColumn(columnIndex) { // TODO: columnIndex -> colNum
+        var letterColumn = [];
+        // TODO: replace this loop with a map
+        // insert consonants
+        var i, key, letter;
+        for (i=0; i < this.consonants.length; i++) {
+            letter = this.consonants[i];
+            key = `${ letter }${ columnIndex }`;
+            letterColumn.push(
+                renderLetter(letter, columnIndex, i)
+            );
+        }
+
+        letterColumn.push(
+            this.renderBlank(columnIndex);
+        );
+
+        for (i=0; i < this.vowels.length; i++) {
+            letter = this.vowels[i];
+            key = `${ letter }${ columnIndex }`;
+            letterColumn.push(
+                renderLetter(letter, columnIndex, i)
+            );
+        }
+
+        return (
+            <Col xs={this.colsPerLetter} > {letterColumn} </Col>
+        );
+    }
+
+    handleLetterClick(letter, columnIndex, letterIndex) {
+        var chosenLetters = this.state.chosenLetters.slice();
+        chosenLetters[columnIndex] = this.letterArray(letter)[letterIndex];
+        this.setState({
+            chosenLetters: chosenLetters
+        });
     }
 
     render() {
-        var choiceColumns = [];
+        var letterColumns = [];
         // divide the letters in the name into columns
-        var colsPerLetter = 2; // TODO for general name lengths: Math.max(Math.ceil(12.0/this.nameLength), 12);
         for (var i=0; i < this.nameLength; i++) {
-            choiceColumns.push(<ChoiceColumn key={i} colKey={i} colsPerLetter={colsPerLetter}/>);
+            letterColumns.push(
+                renderColumn(i);
+            );
         }
         return (
             <div className="name_workshop">
                 <Grid>
                     <Row>
-                    {choiceColumns}
+                    {letterColumns}
                     </Row>
                 </Grid>
             </div>
@@ -27,77 +108,14 @@ class NameWorkshop extends Component {
     }
 }
 
-class ChoiceColumn extends Component {
-    constructor() {
-        super();
-        this.consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
-        this.vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-    }
-
-    render() {
-        var upperLetters = [];
-        var lowerLetters = [];
-        // TODO: replace this loop with a map
-        var i, key;
-        for (i=0; i < this.consonants.length; i++) {
-            key = `${ this.consonants[i] }${ this.props.colKey }`;
-            upperLetters.push(
-                <ClickableLetter letter={this.consonants[i]} key={key} />
-            );
-        }
-        var upperColKey = `u${this.props.colKey}`;
-        var upperColumn =
-            <LetterColumn
-                clickableLetters={upperLetters}
-                key={upperColKey}
-                colsPerLetter={this.props.colsPerLetter}
-            />;
-
-        var blankColKey = `b${this.props.colKey}`;
-        var blankLetters = [<LetterBlank />];
-        var blankColumn =
-            <LetterColumn
-                clickableLetters={blankLetters}
-                key={blankColKey}
-                colsPerLetter={this.props.colsPerLetter}
-            />;
-
-        for (i=0; i < this.vowels.length; i++) {
-            key = `${ this.vowels[i] }${ this.props.colKey }`;
-            lowerLetters.push(
-                <ClickableLetter letter={this.vowels[i]} key={key} />
-            );
-        }
-        var lowerColKey = `l${this.props.colKey}`;
-        var lowerColumn = 
-            <LetterColumn
-                className="lower_column"
-                clickableLetters={lowerLetters}
-                key={lowerColKey}
-                colsPerLetter={this.props.colsPerLetter}
-            />;
-
-        var fullColumn = [upperColumn, blankColumn, lowerColumn];
-
-        return (
-            <Col xs={this.props.colsPerLetter} > {fullColumn} </Col>
-        );
-
-    }
-}
-
-class LetterColumn extends Component {
-    render() {
-        return (
-            <Col xs={this.props.colsPerLetter} > {this.props.clickableLetters} </Col>
-        );
-    }
-};
-
 class ClickableLetter extends Component {
     render() {
         return (
-            <div className="clickable_letter"> <span> {this.props.letter} </span> </div>
+            <div
+                onClick={this.props.onClick}
+                className="clickable_letter">
+                    <span> {this.props.letter} </span>
+            </div>
         );
     }
 };
